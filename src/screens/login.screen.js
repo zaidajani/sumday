ed9eb0e03057c1db59252c78b68e5fd6617a43b8
zaidajani <zaidajani2007@gmail.com>
2023-04-25 10:58:51 +0530

    Making authentication system better

--- src/screens/login.screen.js
+++ src/screens/login.screen.js
@@ -1,34 +1,86 @@
-import React from "react";
-import './../css/style.login.css';
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import "./../css/style.login.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const login = () => {
+    if (!email || !password) {
+      return alert("Please fill in all the fields.");
+    }
+
+    axios
+      .post("http://localhost:4000/auth", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        localStorage.setItem('token', res.data);
+        return navigate('/');
+      })
+      .catch((err) => {
+        return alert(err.response.data);
+      });
+  };
+
   return (
     <>
       <div className="App">
-      <div className="navholder">
-        <nav className="navbar">
-          <div className="right">
-            <Link to='/' className="brand">Sumday</Link>
+        <div className="navholder">
+          <nav className="navbar">
+            <div className="right">
+              <Link to="/" className="brand">
+                Sumday
+              </Link>
+            </div>
+            <div className="left">
+              <p className="logbutton">Login</p>
+              <Link to="/register">
+                <p className="logbutton">Register</p>
+              </Link>
+            </div>
+          </nav>
+        </div>
+        <div className="body">
+          <div
+            className="formholder"
+            style={{
+              height: "380px",
+            }}
+          >
+            <p>Login yourself</p>
+            <div
+              className="form"
+              style={{
+                height: "170px",
+              }}
+            >
+              <input
+                type="text"
+                placeholder="Email"
+                className="inputclass"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="inputclass"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+            <div className="loginbutton" onClick={login}>Login</div>
           </div>
-          <div className="left">
-            <p className="logbutton">Login</p>
-            <Link to="/register"><p className="logbutton">Register</p></Link>
-          </div>
-        </nav>
-      </div>
-      <div className="body">
-        <div className="formholder">
-          <p>Login yourself</p>
-          <div className="form">
-            <input type="text" placeholder="Name" className="inputclass"/>
-            <input type="text" placeholder="Email" className="inputclass"/>
-            <input type="password" placeholder="Password" className="inputclass"/>
-          </div>
-          <div className="loginbutton">Login</div>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
--- src/screens/register.screen.js
+++ src/screens/register.screen.js
@@ -1,8 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import './../css/style.login.css';
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const submitForm = () => {
+    if (!name || !email || !password) {
+      return alert('Please fill in all the fields');
+    }
+    axios
+      .post("http://localhost:4000/user/", {
+        email: email,
+        name: name,
+        password: password
+      })
+      .then((res) => {
+        alert('User created');
+        navigate('/login');
+      })
+      .catch(e => {
+        return alert(e.response.data);
+      });
+  }
+
   return (
     <div className="App">
       <div className="navholder">
@@ -20,11 +46,17 @@ export const Register = () => {
         <div className="formholder">
           <p>Register yourself</p>
           <div className="form">
-            <input type="text" placeholder="Name" className="inputclass"/>
-            <input type="text" placeholder="Email" className="inputclass"/>
-            <input type="password" placeholder="Password" className="inputclass"/>
+            <input type="text" placeholder="Name" className="inputclass" onChange={(e) => {
+              setName(e.target.value);
+            }}/>
+            <input type="text" placeholder="Email" className="inputclass" onChange={(e) => {
+              setEmail(e.target.value);
+            }}/>
+            <input type="password" placeholder="Password" className="inputclass" onChange={(e) => {
+              setPassword(e.target.value);
+            }}/>
           </div>
-          <div className="loginbutton">Register</div>
+          <div className="loginbutton" onClick={submitForm}>Register</div>
         </div>
       </div>
     </div>
